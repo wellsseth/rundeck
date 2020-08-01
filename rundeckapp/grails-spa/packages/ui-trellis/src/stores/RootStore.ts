771c0fb33a3669c5a9d4b17adcf52e4dfe99ce6d
ProTip <greg.zapp@gmail.com>
2020-07-31 19:19:55 -0500

    Model exec state in mobx

--- rundeckapp/grails-spa/packages/ui-trellis/src/stores/RootStore.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/stores/RootStore.ts
@@ -1,13 +1,16 @@
-import { ExecutionOutputStore, ExecutionOutput } from "./ExecutionOutput"
+import { ExecutionOutputStore } from "./ExecutionOutput"
+import { ExecutionStateStore } from './ExecutionState'
 import { WorkflowStore } from './Workflow'
 import { RundeckClient } from "@rundeck/client"
 
 export class RootStore {
     executionOutputStore: ExecutionOutputStore
     workflowStore: WorkflowStore
+    executionStateStore: ExecutionStateStore
 
     constructor(readonly client: RundeckClient) {
         this.executionOutputStore = new ExecutionOutputStore(this, client)
         this.workflowStore = new WorkflowStore(this, client)
+        this.executionStateStore = new ExecutionStateStore(this, client)
     }
 }
